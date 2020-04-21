78d25eb6b042568246b6f0dde05a8437d5ec651c
Dhara Panchal <dhara.panchal@einfochips.com>
2020-04-21 13:46:04 +0530

    add authentication helper method and restrict actions from UI and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 2)
   end
@@ -11,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully login"
       redirect_to articles_path
     else
@@ -19,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "User profile updated successfully."
       redirect_to @user
@@ -33,12 +35,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 end
