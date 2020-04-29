d6ba6d933c902d10ef0eaca56aa24d9bbd64ad46
Dhara Panchal <dhara.panchal@einfochips.com>
2020-04-29 18:38:20 +0530

    add userId in articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_095316) do
+ActiveRecord::Schema.define(version: 2020_04_29_130202) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_29_095316) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "categories", force: :cascade do |t|
