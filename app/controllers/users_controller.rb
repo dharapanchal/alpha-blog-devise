2cd864c5a9afa0a6f216ec4f6dab694e79ba8f91
Dhara Panchal <dhara.panchal@einfochips.com>
2020-04-30 15:17:12 +0530

    updated users controller and users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,46 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 2)
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully login"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User profile updated successfully."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
+  end
+
+  def destroy
+    @user.destroy
+    flash[:danger] = "User and all articles have been deleted"
+    redirect_to users_path
   end
 
   private
@@ -16,6 +51,5 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-
 
 end
