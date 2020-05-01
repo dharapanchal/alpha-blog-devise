1b54ffdb3bdce26e5ef30b9b5181b140214e2bc8
Dhara Panchal <dhara.panchal@einfochips.com>
2020-05-01 19:33:36 +0530

    updated navigation links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,28 +6,13 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 2)
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully login"
-      redirect_to articles_path
-    else
-      render 'new'
-    end
-  end
-
   def edit
   end
 
   def update
     if @user.update(user_params)
       flash[:notice] = "User profile updated successfully."
-      redirect_to @user
+      redirect_to users_path
     else
       render 'edit'
     end
